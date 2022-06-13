d76c0f97fc99e17ac4639b1c6e53c6d6125ef9d4
sultan288 <sultan.ist28@gmail.com>
2022-06-14 02:03:40 +0600

    updateRealProject

---
+++ RealProjectB1/auth/login.aspx.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RealProjectB1.auth
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                divMsg.Visible = false;
+                RememberCookie();
+            }
+           
+        }
+
+        private void RememberCookie()
+        {
+            if (Request.Cookies["UserName"] != null && Request.Cookies["Password"] != null)
+            {
+                txtUsername.Text = Request.Cookies["UserName"].Value;
+                txtPassword.Attributes["Value"] = Request.Cookies["Password"].Value;
+            }
+        }
+
+        protected void btnLogin1_Click(object sender, EventArgs e)
+        {
+            if (CheckFieldValue()==false)
+            {
+                if (txtUsername.Text == "admin" && txtPassword.Text == "123")
+                {
+                    Session["UserId"] = "1";
+                    Session["UserName"] = "Admin";
+                    SetCookie();
+
+                    Response.Redirect("~/AdminHome.aspx");
+                }
+                else
+                {
+                    lblMsg.Text = "Incorrect Username or Password";
+                    divMsg.Visible = true;
+                }
+   
+            }
+            
+        }
+
+        private void SetCookie()
+        {
+            HttpCookie mycookie = new HttpCookie("mycookie");
+            //mycookie["UserName"] = txtUsername.Text.Trim();
+            //mycookie["Password"] = txtPassword.Text.Trim();
+            mycookie["UserName"] ="";
+            mycookie["Password"] = "";
+
+            Response.Cookies.Add(mycookie);
+
+            if (chkRememberMe.Checked)
+            {
+                Response.Cookies["UserName"].Expires = DateTime.Now.AddDays(3);
+                Response.Cookies["Password"].Expires = DateTime.Now.AddDays(3);
+            }
+            else
+            {
+                Response.Cookies["UserName"].Expires = DateTime.Now.AddDays(-1);
+                Response.Cookies["Password"].Expires = DateTime.Now.AddDays(-1);
+            }
+
+            Response.Cookies["UserName"].Value = txtUsername.Text.Trim();
+            Response.Cookies["Password"].Value = txtPassword.Text.Trim();
+
+        }
+
+        private bool CheckFieldValue()
+        {
+            bool IsReq = false;
+
+            if (txtUsername.Text=="")
+            {
+                IsReq = true;
+                lblMsg.Text = "Username can't be empty";
+
+            }
+            else if (txtPassword.Text =="")
+            {
+                IsReq = true;
+                lblMsg.Text = "Password can't be empty";
+            }
+
+            if (IsReq==true)
+            {
+                divMsg.Visible = true;
+            }
+            else
+            {
+                divMsg.Visible = false;
+            }
+
+            return IsReq;
+        }
+
+
+    }
+}
