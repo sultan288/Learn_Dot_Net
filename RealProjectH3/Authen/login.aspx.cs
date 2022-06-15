f2b7d0e6ff7d75dd44c8c40dbf8f0d02f254cb76
sultan288 <sultan.ist28@gmail.com>
2022-06-16 01:11:56 +0600

    update RealprojectH3

---
+++ RealProjectH3/Authen/login.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RealProjectH3.Authen
+{
+    public partial class login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            divMsg.Visible = false;
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (CheckFieldValue()== false)
+            {
+                if (txtUsername.Text == "admin" && txtPassword.Text == "123")
+                {
+                    Response.Redirect("~/Student/StudentRegistration.aspx");
+                }
+            }
+        }
+
+        private bool CheckFieldValue()
+        {
+            bool IsReq = false;
+
+            if (txtUsername.Text == "")
+            {
+                IsReq = true;
+                lblMsg.Text = "Username can't be empty";
+            }
+            else if (txtPassword.Text == "")
+            {
+                IsReq = true;
+                lblMsg.Text = "Password can't be empty";
+            }
+
+            if (IsReq == true)
+            {
+                divMsg.Visible = true;
+            }
+            else
+            {
+                divMsg.Visible = false;
+            }
+
+            return IsReq;
+        }
+    }
+}
---
+++ RealProjectH3/Student/StudentRegistration.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPages/AdminLayoutPage.Master" AutoEventWireup="true" CodeBehind="StudentRegistration.aspx.cs" Inherits="RealProjectH3.Student.StudentRegistration" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    This is student form.
+    This is another form.
+
+
+
+</asp:Content>
